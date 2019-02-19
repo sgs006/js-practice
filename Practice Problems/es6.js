4df86a58b846af26a08fcae62b0778a17004034c
Sam Stokes <sgstokes006@gmail.com>
2019-02-18 19:29:42 -0600

    Destructuring
    Examples of destructuring in es5 and es6

--- Practice Problems/es6.js
+++ Practice Problems/es6.js
@@ -237,3 +237,40 @@ Person.prototype.myFriends6 =
 
 let friends = ['Bob', 'Jane', 'Mark'];
 new Person('John').myFriends6(friends);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Lecture: Destructuring
+
+//ES5
+var john = ['John', 26];
+var name1 = john[0];
+var age1 = john[1];
+
+//ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+let obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+let { firstName, lastName } = obj;
+console.log(firstName);
+console.log(lastName);
+
+let { firstName: a, lastName: b } = obj;
+console.log(a);
+console.log(b);
+
+
+//another example
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
